732c394402b513f92bc5e4b98ae9ccf9c6ccbda7
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-22 15:48:06 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,29 +2,35 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
-        },
+        name: "",
+        age: ""
+      },
       :matriarch => {
-        #your key/value pairs here
-        },
+        name: "",
+        age: ""
+      },
       :hero => {
-        #your key/value pairs here
-        },
+        name: "",
+        age: ""
+      },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
-        },
+        name: "",
+        age: ""
+      },
       :matriarch => {
-        #your key/value pairs here
-        },
+        name: "",
+        age: ""
+      },
       :heroine => {
-        #your key/value pairs here
-        },
+        name: "",
+        age: ""
+      },
       :heroine_friends => []
    }
   }
 
-  
+
 end
