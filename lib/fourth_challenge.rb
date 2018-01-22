9c96264aa85b9efdcabf1d399499495fb09da722
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-22 16:07:17 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -18,6 +18,10 @@ def fourth_challenge
         {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
+
+   epic_tragedy[:montague][:hero][:status] = "dead"
+   epic_tragedy[:capulet][:heroine][:status] = "dead"
+   epic_tragedy
 }
 
 
